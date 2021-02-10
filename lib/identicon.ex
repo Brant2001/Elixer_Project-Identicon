0e2934b9db59cb86b8f3feb84737ee13dd0459d9
Brant Pippin <brant.pippin@gmail.com>
2021-02-10 10:03:07 -0600

    Added initial function doc frames

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -2,11 +2,24 @@ defmodule Identicon do
   @moduledoc """
     Generates a custom Identicon based on a string input
   """
+
+  @doc """
+
+
+  ## Examples
+
+  """
   def name(input) do
     input
     |> hash_input
   end
 
+  @doc """
+
+
+  ## Examples
+
+  """
   def hash_input(input) do
     :crypto.hash(:md5, input)
     |> :binary.bin_to_list()
